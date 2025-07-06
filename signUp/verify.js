b085e6866897391c7aba2cebe3555356d1e625b3
sieun2im <kse040531@naver.com>
2025-07-07 00:05:01 +0900

    verify

---
+++ signUp/verify.js
@@ -0,0 +1,23 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const token = urlParams.get('token');
+
+    if (!token) {
+        alert('토큰이 없습니다.');
+        return;
+    }
+
+    fetch(`/api/members/verify?token=${token}`)
+        .then(response => response.text())
+        .then(result => {
+            if (result === 'Verification success') {
+                window.location.href = 'signUp3.js';
+            } else {
+                alert('인증 실패: ' + result);
+            }
+        })
+        .catch(error => {
+            console.error('에러 발생:', error);
+            alert('서버 요청 중 에러가 발생했습니다.');
+        });
+});
